378f0bb3eaf3034df48bf299a2cc07c9f5a9bd01
MOHAMEDSHABLTK <103379827+MOHAMEDSHABLTK@users.noreply.github.com>
2025-12-16 23:59:38 +0530

    more added

---
+++ multab.sh
@@ -0,0 +1,5 @@
+read -p "Enter number:" n
+for ((i=1; i<=10; i++))
+do
+   echo "$n x $i = $((n*i))"
+done
---
+++ reverse.sh
@@ -0,0 +1,9 @@
+read -p "Enter number:" n
+rev=0
+while [ $n -ne 0 ]
+do
+    rem=$((n%10))
+    rev=$((rev * 10 + rem))
+    n=$((n / 10))
+done
+echo "reversed number=$rev"
