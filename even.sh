3783f9a7dd1151f827ab64110e590aa298acd76e
MOHAMEDSHABLTK <103379827+MOHAMEDSHABLTK@users.noreply.github.com>
2025-12-16 23:22:51 +0530

    2 programs even and leap added

---
+++ even.sh
@@ -0,0 +1,4 @@
+for((i=2; i<=50 ; i+=2))
+do
+    echo $i
+done
---
+++ leap.sh
@@ -0,0 +1,6 @@
+read -p "Enter year:" y
+if (((y % 4 == 0 && y % 100 != 0)||(y % 400 == 0))); then
+    echo "Leap year"
+else
+    echo "Not a leap year"
+fi
